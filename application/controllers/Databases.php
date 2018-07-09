d6acea90ed7ad50a1b2333b26b8b8c6a5e9365e7
CJ Villegas <vchapreljohn@gmail.com>
2018-07-09 23:40:18 +0800

    Migration

--- application/controllers/Databases.php
+++ application/controllers/Databases.php
@@ -53,10 +53,11 @@
 						'fname'=>$value->fname,
 						'lname'=>$value->lname,
 						'mname'=>$value->mname,
+						'fullname'=>$value->lname.' '.$value->fname.' '.$value->mname,
 						'extension'=>$value->extension,
 						'birth'=>$value->birth,
 						'gender'=>$value->gender,
-						'pob'=>$value->place,
+						'pob'=>$value->pob,
 						'civilstatus'=>$value->civilstatus,
 						'profession'=>$value->profession,
 						'mother'=>$value->mother,
@@ -75,29 +76,29 @@
 					if($insert_id){
 						$form_data = array(
 							'profileID'=>$insert_id,
-							'region'=>$value->region,
-							'province'=>$value->province,
-							'city'=>$value->city,
-							'sitio'=>$value->sitio,
-							'brgy'=>$value->brgy
+							'region'=>'REGION X',
+							'province'=>'MISAMIS ORIENTAL',
+							'city'=>'CAGAYAN DE ORO CITY',
+							'sitio'=>'BOLONSORI',
+							'brgy'=>'CAMAMAN-AN'
 						);
 						$insert_id1 = $this->Database->insertAddress($form_data);
-						if($insert_id1){
-							$this->disecter($insert_id,$value->one);
-							$this->disecter($insert_id,$value->two);
-							$this->disecter($insert_id,$value->three);
-							$this->disecter($insert_id,$value->four);
-							$this->disecter($insert_id,$value->five);
-							$this->disecter($insert_id,$value->six);
-							$this->disecter($insert_id,$value->seven);
-							$this->disecter($insert_id,$value->eight);
-							$this->disecter($insert_id,$value->nine);
-							$this->disecter($insert_id,$value->ten);
-							$this->disecter($insert_id,$value->eleven);
-							$this->disecter($insert_id,$value->twelv);
-							$this->disecter($insert_id,$value->tirten);
-							$this->disecter($insert_id,$value->fourteen);
-						}
+						// if($insert_id1){
+						// 	$this->disecter($insert_id,$value->one);
+						// 	$this->disecter($insert_id,$value->two);
+						// 	$this->disecter($insert_id,$value->three);
+						// 	$this->disecter($insert_id,$value->four);
+						// 	$this->disecter($insert_id,$value->five);
+						// 	$this->disecter($insert_id,$value->six);
+						// 	$this->disecter($insert_id,$value->seven);
+						// 	$this->disecter($insert_id,$value->eight);
+						// 	$this->disecter($insert_id,$value->nine);
+						// 	$this->disecter($insert_id,$value->ten);
+						// 	$this->disecter($insert_id,$value->eleven);
+						// 	$this->disecter($insert_id,$value->twelv);
+						// 	$this->disecter($insert_id,$value->tirten);
+						// 	$this->disecter($insert_id,$value->fourteen);
+						// }
 					}
 				}else{
 					//echo "DOne";
